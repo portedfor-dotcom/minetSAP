30eef5ef319016c70b138b02a80978c18d6d1102
portedfor-dotcom <portedfor@gmail.com>
2026-08-17 12:16:59 +1000

    Add files via upload

--- data.js
+++ data.js
@@ -6,11 +6,48 @@
 var DATA = {
   members: [
   {
-    "nick": "ьВЫАЫ",
+    "nick": "Minet",
+    "rank": "heart",
+    "points": "9999",
+    "contact": "-"
+  },
+  {
+    "nick": "Giris_Adam",
+    "rank": "heart",
+    "points": "50",
+    "contact": "@Giris_Adam"
+  },
+  {
+    "nick": "DeBo4Ka_yeHcDey",
+    "rank": "crimson",
+    "points": "40",
+    "contact": "@KickDreams"
+  },
+  {
+    "nick": "TOPdromogor",
+    "rank": "legion",
+    "points": "31",
+    "contact": ""
+  },
+  {
+    "nick": "Lis_overmin",
+    "rank": "legion",
+    "points": "30",
+    "contact": ""
+  },
+  {
+    "nick": "Kolitsiner3",
     "rank": "spark",
-    "points": "",
+    "points": "20",
     "contact": ""
   }
 ],
-  news: []
+  news: [
+  {
+    "title": "Добро пожаловать",
+    "text": "Здравствуйте, уважаемые посетители сайта! Чтобы вступить в клан, обратитесь к игрокам «Багровый Свет» или «Серебряное Пламя». Список участников доступен в реестре.",
+    "img": "",
+    "date": "17.08.2026, 12:16:30"
+  }
+]
 };
