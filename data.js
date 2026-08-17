a67bc15577c05e954bc2f9058719fde1bcd9cce6
portedfor-dotcom <portedfor@gmail.com>
2026-08-17 14:59:10 +1000

    Add files via upload

--- data.js
+++ data.js
@@ -1,11 +1,1 @@
-var DATA={
-  members:[
-    {nick:"minet",rank:"heart",points:"9999",contact:"_"},
-    {nick:"Giris_Adam",rank:"heart",points:"100",contact:"@Giris_Adam"},
-    {nick:"DeBo4Ka_yeHcDey",rank:"crimson",points:"40",contact:"@KickDreams"},
-    {nick:"TOPdromogor",rank:"legion",points:"30",contact:""},
-    {nick:"Lis_overmin",rank:"legion",points:"30",contact:""},
-    {nick:"Kolitsiner3",rank:"temp",points:"10",contact:""}
-  ],
-  news:[]
-};
+var DATA={members:[{"nick":"minet","rank":"heart","points":"9999","contact":"_"},{"nick":"Giris_Adam","rank":"heart","points":"100","contact":"@Giris_Adam"},{"nick":"DeBo4Ka_yeHcDey","rank":"crimson","points":"40","contact":"@KickDreams"},{"nick":"TOPdromogor","rank":"legion","points":"30","contact":""},{"nick":"Lis_overmin","rank":"legion","points":"30","contact":""},{"nick":"Kolitsiner3","rank":"temp","points":"5","contact":""}],news:[]};
